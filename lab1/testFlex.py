4d0aed9b19613e5221b58e0e9acb57e4c8c5e9dd
geokyr <imgeorgekyr@gmail.com>
2021-12-03 18:28:25 +0200

    finished 1 pre-bonus

---
+++ lab1/testFlex.py
@@ -0,0 +1,17 @@
+from subprocess import PIPE, Popen
+
+for n in range(0,34):
+    inputFile = f'teleports/input{n}.txt'
+    outputFile = f'teleports/output{n}.txt'
+    f = open(outputFile)
+    output = f.read()
+    command = "./1Bonus < " + inputFile
+    process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
+    producedOutput, err = process.communicate()
+
+    if output == producedOutput.decode("utf-8"):
+        print(f'+++ Testcase {n} passed!')
+    else:
+        print(f'--- Testcase {n} failed!')
+        print(f'Expected Output:\n{output}')
+        print(f'Produced Output:\n{producedOutput.decode("utf-8")}')
